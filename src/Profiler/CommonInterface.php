45ddbf388adadb8bab87553163911753ecbc8a8e
Rodion Kachkin <kyleRQWS@gmail.com>
2020-11-19 19:22:14 +0400

    Create CommonInterface.php

---
+++ src/Profiler/CommonInterface.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * This file is part of the Qunity package.
+ *
+ * Copyright (c) Rodion Kachkin <kyleRQWS@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Qunity\Component\Profiler;
+
+/**
+ * Interface CommonInterface
+ * @package Qunity\Component\Profiler
+ */
+interface CommonInterface
+{
+    /**
+     * Set config data
+     *
+     * @param array $config
+     * @return self
+     */
+    public function setConfig(array $config): self;
+
+    /**
+     * Get config data / config value
+     *
+     * @param string|null $key
+     * @param null $default
+     *
+     * @return mixed
+     */
+    public function getConfig(string $key = null, $default = null);
+
+    /**
+     * Check is enabled activity
+     * @return bool
+     */
+    public function isEnabled(): bool;
+}
