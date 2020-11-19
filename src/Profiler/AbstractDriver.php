e9e143bf4c7da40c96d34fb9a75c270cb61d46da
Rodion Kachkin <kyleRQWS@gmail.com>
2020-11-19 19:39:07 +0400

    Create AbstractDriver.php and AbstractOutput.php

---
+++ src/Profiler/AbstractDriver.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of the Qunity package.
+ *
+ * Copyright (c) Rodion Kachkin <kyleRQWS@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Qunity\Component\Profiler;
+
+/**
+ * Class AbstractDriver
+ * @package Qunity\Component\Profiler
+ */
+abstract class AbstractDriver extends AbstractCommon implements DriverInterface
+{
+    // abstract class to separate types
+}
