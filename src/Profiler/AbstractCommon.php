cc20188a311c9fae1956ad59fac76cdf789dfc20
Rodion Kachkin <kyleRQWS@gmail.com>
2020-11-19 19:27:28 +0400

    AbstractCommon implements CommonInterface

---
+++ src/Profiler/AbstractCommon.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * This file is part of the Qunity package.
+ *
+ * Copyright (c) Rodion Kachkin <kyleRQWS@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Qunity\Component\Profiler;
+
+/**
+ * Class AbstractCommon
+ * @package Qunity\Component\Profiler
+ */
+abstract class AbstractCommon implements CommonInterface
+{
+    /**
+     * Config data
+     * @var array
+     */
+    protected array $config = [];
+
+    /**
+     * @inheritDoc
+     */
+    public function getConfig(string $key = null, $default = null)
+    {
+        if ($key) {
+            if (isset($this->config[$key])) {
+                return $this->config[$key];
+            } else {
+                return $default;
+            }
+        } else {
+            if ($this->config) {
+                return $this->config;
+            } else {
+                return $default;
+            }
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setConfig(array $config): CommonInterface
+    {
+        $this->config = $config;
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isEnabled(): bool
+    {
+        return $this->config['enabled'] ?? false;
+    }
+}
